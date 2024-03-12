9ea96cfbf1f4c2cc481af900a7ce431eadb6c9e4
adhikariHarshit2211 <159242647+adhikariHarshit2211@users.noreply.github.com>
2024-03-12 08:59:40 +0530

    Add files via upload

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const connectDB = async() => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://harshitadhikari1122:nDxwjZ9AaIjiMJzl@cluster0.io07zq8.mongodb.net/");
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
